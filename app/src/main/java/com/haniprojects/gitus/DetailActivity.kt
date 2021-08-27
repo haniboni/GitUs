72691a4c3a5e36636c220fdf0438b396c81e7fc9
haniboni <haninuraini980@gmail.com>
2021-08-27 14:30:59 +0700

    Setup view binding

--- app/src/main/java/com/haniprojects/gitus/DetailActivity.kt
+++ app/src/main/java/com/haniprojects/gitus/DetailActivity.kt
@@ -2,10 +2,15 @@ package com.haniprojects.gitus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.haniprojects.gitus.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/com/haniprojects/gitus/GetStartedActivity.kt
+++ app/src/main/java/com/haniprojects/gitus/GetStartedActivity.kt
@@ -2,10 +2,15 @@ package com.haniprojects.gitus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.haniprojects.gitus.databinding.ActivityGetStartedBinding
 
 class GetStartedActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityGetStartedBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_get_started)
+        binding = ActivityGetStartedBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
