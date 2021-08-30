5e190ac6f28b4ac71f746dc7f18efa1d8eeae612
haniboni <haninuraini980@gmail.com>
2021-08-30 11:04:28 +0700

    Change Requirement Fixing

--- app/src/main/java/com/haniprojects/gitus/User.kt
+++ app/src/main/java/com/haniprojects/gitus/User.kt
@@ -1,14 +1,15 @@
 package com.haniprojects.gitus
 
 import android.os.Parcelable
+import androidx.annotation.DrawableRes
 import kotlinx.android.parcel.Parcelize
 
 
 @Parcelize
 data class User(
+    var avatar: Int,
     var name: String,
     var username: String,
-    var avatar: String,
     var location: String,
     var repository: String,
     var company: String,
